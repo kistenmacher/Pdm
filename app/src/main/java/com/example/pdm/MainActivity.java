95fd55d5e2054b012e4f64d677359e84c60f88a6
kistenmacher <jeankm5@hotmail.com>
2020-03-04 21:54:21 -0300

    First Commit

--- app/src/main/java/com/example/pdm/MainActivity.java
+++ app/src/main/java/com/example/pdm/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.pdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int i = 0;
+        btn = findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), OutraActivity.class);
+                abrirTelaNova(v);
+            }
+        });
+
+        Log.d("CICLO", "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("CICLO", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("CICLO", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("CICLO", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("CICLO", "onDestroy");
+    }
+
+    public void abrirTelaNova(View view) {
+        Intent intent = new Intent(this, OutraActivity.class);
+    }
+
 }
